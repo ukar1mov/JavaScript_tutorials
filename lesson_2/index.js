313ae892c78f2bae6cc7b7fd5f999de8cbb9a6c1
ukar1mov <ulugbekbekkarimov@gmail.com>
2023-02-24 02:28:09 +0500

    lesson_2

--- lesson_2/index.js
+++ lesson_2/index.js
@@ -1,2 +1,68 @@
-// dusuc
-// dhufa
+// =============Type Conversions===========
+// let a = 1;
+// let b = "1";
+// console.log(a + b);
+// let a = 1;
+// let b = "1";
+// console.log(a + +b);
+// let a = "Ulug'bek"
+// let b = " Karimov"
+// console.log(a + b);
+// let a = "2004"
+// console.log(+a);
+// let a = "2004s"
+// console.log(+a); terminal: NaN
+// let a = "2004s";
+// console.log(isNaN(a)); terminal: true
+// let b = "2004";
+// console.log(isNaN(b)); terminal: false
+// let a = "50";
+// console.log(Number(a)); terminal: 50 in Number
+// let a = "50";
+// console.log(parseInt(a)); terminal: 50 in Number
+// let a = "50";
+// console.log(parseFloat(a)); terminal: 50 in Number
+// Number(), parseInt(), parseFloat() => boshqa turdagi data typlarni numberga o'zgartiradi.
+// Bularning farqi shundaki, parseInt() bizga sonning faqat yaxlit qsmini olib beradi, kars qisimni ciqarmaydi.
+// Number() bilan parseFloat() esa, ikkolovi deyarlik bir xil, lekn parseFloat() raqaminig qayerida xarf bulsa ushetigaca olib beradi.
+// let a = "200.20";
+// console.log(Number(a)); terminal: 200.20
+// let b = "200.20";
+// console.log(parseInt(b)); terminal: 200
+// let c = "200.20";
+// console.log(parseFloat(c)); terminal: 200.20
+// let a = "30d";
+// console.log(Number(a)); terminal: NaN
+// let b = "30d";
+// console.log(parseFloat(b)); terminal: 30
+// ==================================End type conversion
+// ================================comparison
+// true => !0, !undefined, !null
+// console.log(false == 0); terminal: true
+// console.log(true == 1); terminal: true
+// console.log(null == 0); terminal: false
+// console.log(null == false); terminal: false
+// console.log(null == undefined); terminal: true
+// console.log(undefined == false); terminal: false
+// console.log(true == "1"); terminal: true
+// console.log(true == "a"); terminal: false
+// console.log(true == "2"); terminal: false
+// console.log(true == "0"); terminal: false. lekn if else da true chiqadi
+// console.log(true == "webbrain");
+// console.log(false == "webbrain");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
