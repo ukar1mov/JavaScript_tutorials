1eb1795bba8d1420fcc6f82b4832bb40b34fd111
ukar1mov <ulugbekbekkarimov@gmail.com>
2023-02-24 20:57:51 +0500

    lesson_3

---
+++ lesson_3/index.js
@@ -0,0 +1,51 @@
+// ====================if else===================
+// 0, null, undefined
+//  let temp =  "sardor";  //issiq, sovuq, yomgir, qor
+// if(temp === "issiq"){
+//     console.log("Havo issiq, yengil kiyinib oling");
+// }else if(temp === "sovuq") {
+//     console.log("Havo sovuq, qalin kiyinib oling");
+// }else if(temp === "yomgir"){
+//     console.log("Yomg'r, soyabon oling");
+// }else if(temp === "qor"){
+//     console.log("Qor, ko'chaga chiqmang!");
+// }else{
+//     console.log(`ma'lumot yo'q!`);
+// }
+// ########### wheather forecast######################################
+// let temp = -5; //temp > 0 && temp < 10, temp > 10 && temp < 15, temp > 15 && temp <25
+// if(temp < 0){
+//     console.log("juda sovuq");
+// }
+//  else if(temp >= 0 && temp <= 10){
+//     console.log("sovuq");
+// }else if(temp >= 10 && temp < 15){
+//     console.log("iliq havo");
+// }else if(temp >= 15 && temp <= 25){
+//     console.log("havo issiq");
+// }else if(temp > 25){
+//     console.log("juda issiq ob havo");
+// }
+// else{
+//     console.log("no ma'lum");
+// }
+// =================================if else without brackets==========
+//  if (condition) console.log("havo juda issiq");
+//  else if (condition) return true;
+//  else return false;
+// =======================================
+// ================================ternaty operators==========
+// ? : => ternary operator
+// let temp = 100;
+// temp > 0 ? console.log("havo sovuq"):console.log("not found");
+//  multi state checking
+// let temp = 20;
+// temp >= 0 && temp <= 10 
+// ? console.log("havo sovuq") 
+// :temp > 10 && temp <= 15 
+// ?console.log("havo iliq") 
+// :temp > 15 && temp <=20
+// ?console.log("havo issiq")
+// : console.log("not found"); 
+
+
