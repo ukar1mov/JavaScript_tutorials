ab8e6cc2f52cc84c956d3749c7c5e8646d6c6748
ukar1mov <ulugbekbekkarimov@gmail.com>
2023-02-25 06:05:19 +0500

    lesson_3

--- lesson_3/index.js
+++ lesson_3/index.js
@@ -47,5 +47,50 @@
 // :temp > 15 && temp <=20
 // ?console.log("havo issiq")
 // : console.log("not found"); 
+// ======================Nullish==========
+// nullish => null, undefined
+// console.log(null ?? 1); terminal: 1
+// console.log(undefined ?? 2); terminal: 2
+// console.log(null ?? undefined); terminal: undefined
+// console.log(2 * 2 *2); terminal: 8
+// console.log(2 * 2 * 2 * 2); terminal: 16
+// console.log(2 ** 4); treminal: 16
+// ================================EcmaScript========================
+// let a = "EcmaScript";
+// console.log("What's the official name of JavaScript?");
+// if (a === "EcmaScript"){
+//     console.log("Right!");
+// }else{
+//     console.log(`You don't know?
+//    "EcmaScript"`);
+// }
+// =============================================
+// ======================================0==============
+// let a = 0;
+// if (a > 0){
+//     console.log(1);
+// }else if(a < 0){
+//     console.log(-1);
+// }else{
+//     console.log(0);
+// }
+// =======================================
+// let a = 2;
+// if (a < 4){
+//     console.log("below");
+// }else{
+//     console.log("over");
+// }
+// =============================login
+// let login = "Employee";
+// if (login === "Employee"){
+//     console.log("Hello!");
+// }else if(login === "Director"){
+//     console.log("Greetings");
+// }else if(login === ""){
+//     console.log("no login!");
+// }else{
+//     console.log("");
+// }
 
 
