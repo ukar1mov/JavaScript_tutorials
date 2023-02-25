c2461964b60153230564ab45a098fc837b9c01b9
ukar1mov <ulugbekbekkarimov@gmail.com>
2023-02-25 07:08:48 +0500

    switch case, for loop, do while

---
+++ lesson_4/index.js
@@ -0,0 +1,68 @@
+// ===================Switch case
+// if (condition)
+// switch (variable)
+// let a = "yomgir"
+// switch (a){
+//     case "yomgir":
+//         console.log("bugun yomgir");
+// }
+// let a = "shamol"
+// switch(a){
+//     case "yomgir":
+//         console.log("bugun yomgir");
+//         break;
+//     case "qor":
+//         console.log("bugun qor");
+//         break;
+//     case "shamol":
+//         console.log("bugun shamol");
+//         break;
+//     case "to'fon":
+//         console.log("bugun to'fon");
+//         break;
+//     default: console.log("noma'lum");
+// }
+// ================================For loop
+// let i = 1;
+
+// for (i; i <= 10; i++){
+//     console.log(i);
+// }
+
+// for (let i = 0; i >= -10; i--){
+//     console.log(i);
+// }
+
+// let sum = 0;
+// for (i= 1; i < 12; i++){
+//     console.log(sum += i);
+//     if (sum === 10) break;
+// }
+
+// for (i = 1; i < 12; i++){
+//     console.log(i);
+//     i += i;
+// }
+
+// for (i = 1; i < 12; i++){
+//     console.log(i);
+//     i = i * 2;
+// }
+// for (i = 1; i < 12; i++){
+//     if(i % 2 === 0){
+//         console.log(i);
+//     }
+// }
+
+for (i = 1; i < 12; i++){
+    if (i % 2 !== 0){
+        console.log(i);
+    }
+}
+
+
+
+
+
+
+
