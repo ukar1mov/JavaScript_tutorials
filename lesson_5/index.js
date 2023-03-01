3bc089822a2334b17ee56380e423d55ebf122859
ukar1mov <ulugbekbekkarimov@gmail.com>
2023-03-01 05:59:15 +0500

    while, do while loops, function basics

--- lesson_5/index.js
+++ lesson_5/index.js
@@ -1,1 +1,53 @@
 // lesson 5
+// ====================while, do while loops===================
+// let i = 3;
+// while(i){
+//     console.log(i);
+//     i--;
+
+// // }
+// let i = 3;
+// do{
+//     console.log(i);
+// }while(i--)
+
+
+
+
+
+
+// ==========================function=====================
+// function getAvr(){
+//     console.log("Ulug'bek");
+//     console.log("Ulug'bek");
+//     console.log("Ulug'bek");
+//     console.log("Ulug'bek");
+//     console.log("Ulug'bek");
+//     console.log("Ulug'bek");
+//     console.log("Ulug'bek");
+// }
+
+// getAvr();
+// function getAvg(){
+//     console.log(`Ulug'bek, welcome to GOOGLE!`);
+// }
+
+// getAvg();
+
+// const getAvg = function(){
+//     console.log(`Ulug'bek, welcome to the US!`);
+// }
+
+// getAvg();
+
+// const getAvg = ()=>{}; => arrow function
+// const getAvg = function(name){
+//     console.log(`${name}, welcome to America!`);
+// }
+
+// getAvg(`Ulug'bek`);
+// getAvg(`Nurbek`);
+// getAvg(`Asilbek`);
+// getAvg(`Diyorbek`);  
+
+
