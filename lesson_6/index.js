921739bdbfbbfafee151d6ed317d5b5f831b0360
ukar1mov <ulugbekbekkarimov@gmail.com>
2023-03-03 09:26:17 +0500

    objetc

---
+++ lesson_6/index.js
@@ -0,0 +1,39 @@
+// min(2, 5) 
+// min(3, -1)
+// min(1, 1)
+
+
+// pow(3, 2)
+// pow(3, 3)
+// pow(3, 100)
+
+
+// const findLeast = (a, b)=>{
+//     return(a < b)?console.log(a):console.log(b);
+// }
+// console.log(findLeast(3, -1));
+
+
+
+// const calcPower = (a, b) => {
+//     return(a ** b);
+// }
+// console.log(calcPower(5, 2));
+// console.log(null>=0);
+// console.log(null<=0);
+
+// ========================Objects================
+// {key, indicator, name : value}=> object
+
+// let account = {name: `Ulug'bek`, surname:`Karimov`, wasBorn:`2004`};
+// console.log(account);
+// let newObj1 = {};
+// let newObj2 = new Object();
+// console.log(newObj1, newObj2);
+
+// let account = {name: `Ulug'bek`, surname:`Karimov`, age: 18};
+// console.log(account.surname);
+// console.log(account[`name`]);
+
+
+
