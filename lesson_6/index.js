8a01c38a9202831fb4fa662004fcbdb9a778ba1c
ukar1mov <ulugbekbekkarimov@gmail.com>
2023-03-04 20:17:27 +0500

    objects

--- lesson_6/index.js
+++ lesson_6/index.js
@@ -36,4 +36,180 @@
 // console.log(account[`name`]);
 
 
+// let account = {
+//     name: "Webbrain Academy",
+//     major: "Frontend, bunker",
+//     foundede: 2020, 
+//     "full name": "Webbrain IT Academy", 
+//     0: 23222,
+// };
+// let key = "major";
+// console.log(account.key);
+
+
+// let account = {
+//     name: "Webbrain Academy",
+//     major: "Frontend, bunker",
+//     foundede: 2020, 
+//     "full name": "Webbrain IT Academy", 
+//     0: 23222,
+// };
+// let key = "major";
+// console.log(account[key]);
+
+// let account = {
+//     name: "Webbrain Academy",
+//     major: "Frontend, bunker",
+//     foundede: 2020, 
+//     "full name": "Webbrain IT Academy", 
+//     0: 23222,
+// };
+// let key = "major";
+// console.log(account["founded"]);
+
+// let account = {
+//     name: "Webbrain Academy",
+//     major: "Frontend, bunker",
+//     foundede: 2020, 
+//     "full name": "Webbrain IT Academy", 
+//     0: 23222,
+// };
+// let name = "major";
+// console.log(account.name);
+// console.log(account[name]);
+// let account = {
+//     name: "Webbrain Academy",
+//     major: "Frontend, bunker",
+//     foundede: 2020, 
+//     "full name": "Webbrain IT Academy", 
+//     0: 23222,
+// };
+// let name = "major";
+// console.log(account["name"]);
+// let account = {
+//     name: "Webbrain Academy",
+//     major: "Frontend, bunker",
+// };
+
+// account.name = "Karimov Ulug'bek"
+
+// console.log(account.name);
+// let account = {
+//     name: "Webbrain Academy",
+//     major: "Frontend, bunker",
+// };
+
+// account.name = "Karimov Ulug'bek"
+// account.born = 2004
+// console.log(account);
+// let account = {
+//     name: "Webbrain Academy",
+//     major: "Frontend, bunker",
+//     name: "Parampam"
+// };
+
+
+// console.log(account.name);
+// =============how to delete object 
+// let account = {
+//         name: "Webbrain Academy",
+//         major: "Frontend, bunker",
+//     };
+//     account.founded = 2020;
+//     delete account.major;
+//     console.log(account);
+    // ===============Object freeze
+    // const account = {
+    //     name: "WebBrain Academy",
+    //     major: "Front-end",
+    // };
+    // Object.freeze(account);
+    // account.founded = 2020;
+    // account.name = "Salom"
+    // delete account.major;
+    // console.log(account);
+
+    // const account = {
+    //         name: "WebBrain Academy",
+    //         major: "Front-end",
+    //     };
+    //     Object.seal(account);
+    //     account.founded = 2020;
+    //     account.name = "Salom"
+    //     delete account.major;
+    //     console.log(account);
+
+
+//  const ac1 = {
+//     name: "WebBrain Academy",
+//     major: "Front-End development",
+//     founded: 2020,
+//  };
+
+//  const ac2 = {
+//     name: "WebBrain Academy",
+//     major: "Front-End development",
+//     founded: 2020,
+//  };
+
+
+//  console.log(ac1 === ac2);
+
+// const ac1 = {
+//     name: "WebBrain Academy",
+//     major: "Front-End development",
+//     founded: 2020,
+//  };
+
+//  const ac2 = structuredClone(ac1);
+
+
+//  console.log(ac2);
+// let name = "Webbrain Academy";
+// let major = "Frontend";
+// const ac1 = {
+//     name,
+//     major: major,
+//     founded: 2020,
+//  };
+
+ 
+
+
+//  console.log(ac1);
+
+// const ac1 = {
+//     name: "Webbrain Academy",
+//     major: "Frontend",
+// };
+
+// console.log("name" in ac1);
+
+// const ac1 = {
+//     name: "Webbrain Academy",
+//     major: "Frontend",
+// }
+
+// const ac2 = {};
+
+// ac1.name = "Parampam"
+
+// Object.assign(ac2, ac1);
+// console.log(ac1, ac2);
+
+// =============================how to get value=========================
+// const ac1 = {
+//     name: "Webbrain Academy",
+//     major: "Frontend",
+//     students: 2000,
+//     founded: 2020,
+//     mantors: 5,
+// };
+
+// for(let i in ac1){
+//     console.log(`${i}: ${ac1[i]}`);
+// }
+
+
+
 
